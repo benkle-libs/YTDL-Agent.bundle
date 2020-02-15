0ec06560b6276cfd54ef0f57cfba9d3ca9276170
Benjamin Kleiner <bizzl@benkle.info>
2020-02-15 10:19:39 +0100

    Initial commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,4 @@
+def Start():
+	HTTP.CacheTime = CACHE_1MONTH
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.54'
+	HTTP.Headers['Accept-Language'] = 'en-us' 
